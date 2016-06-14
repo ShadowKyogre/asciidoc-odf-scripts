346692fd0fd64baddc7c4ea9ae7caaea3e2058c8
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-06-13 18:04:34 -0700

    Allow embedding remote css files

--- asciidoc-out-utils/embed-css.py
+++ asciidoc-out-utils/embed-css.py
@@ -3,6 +3,7 @@
 from lxml.html import fromstring, tostring, Element
 import sys
 from os import path, environ
+from urllib import request, parse as urlparse
 import re
 
 if len(sys.argv) == 1:
@@ -16,16 +17,33 @@ with target  as f:
 
 	for element in root.iter('link'):
 		if element.attrib['rel'] == 'stylesheet' and element.attrib['type'] == 'text/css':
-			if target is sys.stdin:
-				cssfp = path.join(environ['OTHER_SHEETS'], element.attrib['href'])
-			else:
-				cssfp = path.join(path.dirname(sys.argv[1]), element.attrib['href'])
+			remote = False
 
-			with open(cssfp) as cssf:
-				cssf_contents = cssf.read()
+			src_css = element.attrib['href']
+			if urlparse.urlparse(src_css).scheme:
+				remote = True
+
+			if target is sys.stdin:
+				if not remote:
+					cssfp = path.join(environ['OTHER_SHEETS'], src_css)
+				else:
+					cssfp = src_css
+			else:
+				if not remote:
+					cssfp = path.join(path.dirname(sys.argv[1]), src_css)
+				else:
+					cssfp = src_css
+
+
+			if remote:
+				with request.urlopen(cssfp) as cssf:
+					cssf_contents = cssf.read().decode('utf-8')
+			else:
+				with open(cssfp) as cssf:
+					cssf_contents = cssf.read()
 
 			parent = element.getparent()
-			element.attrib['data-href']=element.attrib['href']
+			element.attrib['data-href']=src_css
 			del element.attrib['rel']
 			del element.attrib['href']
 			element.tag = 'style'
