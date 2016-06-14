116a2db3a462775de57b1915e0f94872286196c9
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-06-13 17:52:27 -0700

    Preserve embed order

--- asciidoc-out-utils/embed-css.py
+++ asciidoc-out-utils/embed-css.py
@@ -17,19 +17,19 @@ with target  as f:
 	for element in root.iter('link'):
 		if element.attrib['rel'] == 'stylesheet' and element.attrib['type'] == 'text/css':
 			if target is sys.stdin:
-				with open(path.join(environ['OTHER_SHEETS'], element.attrib['href'])) as cssf:
-					cssf_contents = cssf.read()
-					inlined = Element('style', {'type': 'text/css'})
-					inlined.text = cssf_contents
+				cssfp = path.join(environ['OTHER_SHEETS'], element.attrib['href'])
 			else:
-				with open(path.join(path.dirname(sys.argv[1]), element.attrib['href'])) as cssf:
-					cssf_contents = cssf.read()
-					inlined = Element('style', {'type': 'text/css'})
-					inlined.text = cssf_contents
+				cssfp = path.join(path.dirname(sys.argv[1]), element.attrib['href'])
+
+			with open(cssfp) as cssf:
+				cssf_contents = cssf.read()
 
 			parent = element.getparent()
-			parent.remove(element)
-			parent.append(inlined)
+			element.attrib['data-href']=element.attrib['href']
+			del element.attrib['rel']
+			del element.attrib['href']
+			element.tag = 'style'
+			element.text = '\n{0}'.format(cssf_contents)
 
 	print(
 		tostring(root, pretty_print=True).decode('utf-8')
